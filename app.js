9dbdcfc2bf70b33bd5a3e633e739fe8a44e7de56
joshkohane <joshkohane@gmail.com>
2021-04-08 15:33:41 -0400

    Build out requests

--- app.js
+++ app.js
@@ -4,12 +4,18 @@ const app = express();
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
 
+const questions = require('./routes/api/questions');
+
+app.use(express.json());
+
 mongoose
     .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Mongo is up'))
-    .catch(err => console.log(err))
+    .catch(err => console.log(err));
 
+    
 app.get('/', (req, res) => res.send('This works, huzzah!'));
+app.use('/api/questions', questions);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is serving on port ${port}`));
